f7930606a6c749caa74a62de8b0f8053be195c29
kuyr <kuyeemmanuel@rocketmail.com>
2019-10-07 20:53:31 +0100

    Submitted

---
+++ Carbon_POM_automation(Kuye)/project/Pages/viewTransactionPage.py
@@ -0,0 +1,37 @@
+import os
+import time
+from project.Locators.locators import Locators
+
+
+class ViewTransactionPage():
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.view_wallet_transaction_button_id = Locators.view_wallet_transaction_button_id
+        self.title_menu_button_id = Locators.title_menu_button_id
+        self.date_range_from_button_id = Locators.date_range_from_button_id
+        self.date_range_to_button_id = Locators.date_range_to_button_id
+        self.select_date_button_id = Locators.select_date_button_id
+
+    def click_transaction_button(self):
+        self.driver.find_element_by_id(Locators.view_wallet_transaction_button_id).click()
+
+    def select_correct_dates_from(self):
+        self.driver.find_element_by_id(Locators.title_menu_button_id).click()
+
+        self.driver.find_element_by_id(Locators.date_range_from_button_id).click()
+        self.driver.find_element_by_id(Locators.select_date_button_id).click()
+
+    def select_correct_date_to(self):
+        self.driver.find_element_by_id(Locators.date_range_to_button_id).click()
+        self.driver.find_element_by_id(Locators.select_date_button_id).click()
+
+    def screenshot_and_save(self):
+        directory = '%s/' % os.chdir('../Screenshots/')
+        file_name = 'screenshot.png'
+        ts = time.strftime("%Y_%m_%d_%H:%M:%S")
+        self.driver.save_screenshot(ts + file_name)
+
+
+
+
---
+++ Carbon_POM_automation(Kuye)/project/README.md
@@ -0,0 +1,12 @@
+#### **Carbon Android App Automation Implementation**
+
+I made use of the Page Object Model that has different directories; the Locators, Pages, Tests and Screenshots
+with the unittest python testing framework\
+`Before running: run on the terminal:` pip install Appium-Python-Client\
+You could also use: source venv/bin/activate to navigate into the virtual environment\
+To run the test simply run the: **_project/Tests/mainTest.py_** file in the Tests folder 
+with **_python -m unittest mainTest.py_** it runs and saves a screen-shot of the third test
+in the screenshot folder.
+
+I really liked this task mainly cause of the actual implementation and the AUT was really good. 
+
